89cb3451261c4b1c03ecc7b2c88c70457ecdf8e2
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-09 10:54:32 -0400

    Draft first working version

--- main.js
+++ main.js
@@ -4,7 +4,59 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const glyphs = document.getElementsByClassName("like-glyph");
+const errorDiv = document.getElementById('modal');
+const errorP = document.getElementById('modal-message');
 
+errorDiv.className = 'hidden';
+errorP.innerHTML = '';
+
+console.log(glyphs);
+console.log(glyphs.length);
+for (let i = 0; i < glyphs.length; i++) {
+  glyphs[i].innerHTML = EMPTY_HEART;
+  glyphs[i].classList.remove('activated-heart');
+}
+// glyphs.forEach( function(glyph) {
+//   glyph.innerHTML = EMPTY_HEART;
+// });
+
+for (let i = 0; i < glyphs.length; i++) {
+  glyphs[i].addEventListener('click', event => {
+    const glyphSpan = event.target;
+    const glyphState = glyphSpan.classList.contains('activated-heart');
+    console.log(!glyphSpan);
+    if (!glyphState) {
+      mimicServerCall()
+      .then( () => {
+        glyphSpan.innerHTML = FULL_HEART;
+        glyphSpan.classList.add('activated-heart');
+        errorDiv.className = 'hidden';
+      })
+      .catch( e => {
+        errorDiv.className = '';
+            console.log(e);
+        errorP.innerHTML = e;
+      });
+    } else if (glyphState) {
+      mimicServerCall()
+      .then( () => {
+        glyphSpan.innerHTML = EMPTY_HEART;
+        glyphSpan.classList.remove('activated-heart');
+        errorDiv.className = 'hidden';
+      })
+      .catch( e => {
+        errorDiv.className = '';
+            console.log(e);
+        errorP.innerHTML = e;
+      });
+    } else {
+      alert('Error: Something must have gone wrong.')
+    }
+  });
+}
+
+// function listen() {}
 
 
 //------------------------------------------------------------------------------
